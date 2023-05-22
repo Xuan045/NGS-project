a1c0d0be4605b7e40cfa54fc04848d6995fd85bd
Xuan045 <judychou60@gmail.com>
2023-05-22 14:20:01 +0800

    Separate SRA to 5 parts to download files in parallel

--- download_files.sh
+++ download_files.sh
@@ -1,26 +1,20 @@
 #!/bin/bash
 
 # Set the path to your CSV file
-csv_file="/staging/biology/u4432941/SRA/sra_result.csv"
+csv_file="/staging/biology/u4432941/SRA/sra_partae"
 
 # Download settings
 download_dir="/staging/biology/u4432941/SRA/download_files"
 export PATH=$PATH:/work/opt/ohpc/Taiwania3/pkg/biology/SRAToolkit/sratoolkit_v3.0.3/bin
 
-# Read the CSV file and store each line in an array
-mapfile -t lines < "$csv_file"
-
 cd $download_dir
-# Iterate over each line (excluding the header line)
-for ((i = 1; i < ${#lines[@]}; i++)); do
+# Read the lines from the list
+while IFS= read -r line; do
     # Extract the last column from the line
-    line=${lines[$i]}
     sample_name=$(echo "$line" | awk -F',' '{print $NF}' | tr -d '\r\n')
 
     # Download files using SRAToolkit fastq-dump (download as fastq format and split into R1 and R2)
-    fastq-dump --split-files $sample_name
-#    prefetch $sample_name
+    fastq-dump --gzip --split-files "$sample_name"
+    
     wait
-
-done
-
+done < $csv_file
